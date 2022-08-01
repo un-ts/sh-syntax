c2340d668d4b6e40c9b723afcd137a44ddddaa80
John Armstrong <20903247+jlarmstrongiv@users.noreply.github.com>
2022-08-01 18:25:55 -0400

    fix: only polyfill globalThis.crypto if undefined
    Resolves https://github.com/un-ts/prettier/issues/216

--- src/shim.ts
+++ src/shim.ts
@@ -3,9 +3,11 @@ import _fs from 'node:fs'
 import { performance } from 'node:perf_hooks'
 
 globalThis.fs = _fs
-globalThis.crypto = {
-  // @ts-expect-error
-  getRandomValues: randomFillSync,
+if (!globalThis.crypto) {
+  globalThis.crypto = {
+    // @ts-expect-error
+    getRandomValues: randomFillSync,
+  }
 }
 // @ts-expect-error
 globalThis.performance = performance
