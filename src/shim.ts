4bc3a3ade6663ad50a7d733ac596a9f32d362dd6
JounQin <admin@1stg.me>
2025-04-07 14:14:02 +0800

    refactor: use `??=` instead

--- src/shim.ts
+++ src/shim.ts
@@ -4,18 +4,12 @@ import { randomFillSync } from 'node:crypto'
 import _fs from 'node:fs'
 import { performance } from 'node:perf_hooks'
 
-if (!globalThis.fs) {
-  globalThis.fs = _fs
-}
+globalThis.fs ??= _fs
 
-if (!globalThis.crypto) {
-  globalThis.crypto = {
-    // @ts-expect-error -- incompatible types
-    getRandomValues: randomFillSync,
-  }
-}
-
-if (!globalThis.performance) {
+globalThis.crypto ??= {
   // @ts-expect-error -- incompatible types
-  globalThis.performance = performance
+  getRandomValues: randomFillSync,
 }
+
+// @ts-expect-error -- incompatible types
+globalThis.performance ??= performance
